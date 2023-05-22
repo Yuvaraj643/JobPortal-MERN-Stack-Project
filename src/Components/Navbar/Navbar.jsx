3f9c33ebc5d0bdadd8d5e1f8e31fa7be0e70082f
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-05-22 20:05:32 +0530

    now

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
           Job Portal
         </a>
         <button
